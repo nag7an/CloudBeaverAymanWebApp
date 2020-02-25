9832ae04100b42d80d88d63777bcdd2bc66fdc03
nag7an@outlook.com <nag7an@outlook.com>
2020-02-25 10:35:48 +0400

    Test

---
+++ CloudBeaverAymanWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CloudBeaverAymanWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
